82505c76ac9baa58af27540d99668ed19b74d1ff
LilGreco <74212900+LilGreco@users.noreply.github.com>
2020-11-09 23:05:24 +0200

    Add files via upload

---
+++ puzzleSelect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+
+public class puzzleSelect : MonoBehaviour
+{
+    public void SetPuzzlesPhoto(Image Photo)
+    {
+        for (int i = 0;1 < 36; i++)
+        {
+            GameObject.Find("Piece (" + i + ")").GetComponent<SpriteRenderer>().sprite = Photo.sprite;
+        }
+    }
+
+}
+
